09579a720434586d2325804b57e9ad0d71a3f7bf
cromeero <Cromero24@aoiths.org>
2023-05-02 12:49:11 -0400

    5.3 done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class GameManager : MonoBehaviour
 {
      public List<GameObject> targets; 
       private float spawnRate = 3;
       private int score;
       public TextMeshProUGUI scoreText;
+       public TextMeshProUGUI gameOverText;
+       public bool isGameActive;
+
+      public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
+        isGameActive = true;
+      
         
         UpdateScore(0);
     }
-
+    
+    public void GameOver() 
+    {
+       gameOverText.gameObject.SetActive(true);
+       isGameActive = false;   
+       restartButton.gameObject.SetActive(true);
+    }
 
     IEnumerator SpawnTarget () 
     {
-        while(true)
+        while(isGameActive)
         {
           yield return new WaitForSeconds(spawnRate);
           int Index = Random.Range(0, targets.Count);
@@ -38,4 +52,5 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Target : MonoBehaviour
 {
@@ -40,13 +41,20 @@ public class Target : MonoBehaviour
     }
      private void OnMouseDown() 
      {
-      Destroy(gameObject);
-      Instantiate(explosionParticle, transform.position,explosionParticle.transform.rotation);
-      gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position,explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
      }
      private void OnTriggerEnter(Collider other)
      {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) 
+        { 
+            gameManager.GameOver();
+        }
      }
     // Update is called once per frame
     void Update()
@@ -54,4 +62,8 @@ public class Target : MonoBehaviour
        
     }
     
+    public void RestartGame() 
+    {
+     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
